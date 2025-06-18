6bb00aa11c65036a4127781a04a7334b0e476ae7
Avinash Anand <36325275+anand-avinash@users.noreply.github.com>
2025-06-18 18:51:12 +0200

    added test for LBSim_InvNoiseCovLO_Circulant

--- tests/test_LBSim_noise_ops.py
+++ tests/test_LBSim_noise_ops.py
@@ -22,19 +22,19 @@ import litebird_sim as lbs  # noqa: E402
 
 
 class lbsim_simulation:
-    def __init__(self, nside):
+    def __init__(self):
         self.comm = lbs.MPI_COMM_WORLD
         tmp_dir = tempfile.TemporaryDirectory()
 
         self.sim = lbs.Simulation(
             base_path=tmp_dir.name,
             start_time=234,
-            duration_s=351,
+            duration_s=71,  # Do not increase this number
             random_seed=65454,
             mpi_comm=self.comm,
         )
 
-        detector_list = [
+        self.detector_list = [
             lbs.DetectorInfo(name="det1", sampling_rate_hz=1, net_ukrts=1.0),
             lbs.DetectorInfo(name="det2", sampling_rate_hz=1, net_ukrts=2.0),
             lbs.DetectorInfo(name="det3", sampling_rate_hz=1, net_ukrts=3.0),
@@ -54,21 +54,26 @@ class lbsim_simulation:
             n_block_time = self.comm.Get_size()
 
         self.sim.create_observations(
-            detectors=detector_list,
+            detectors=self.detector_list,
             num_of_obs_per_detector=3,
             n_blocks_det=n_block_det,
             n_blocks_time=n_block_time,
             split_list_over_processes=False,
         )
 
+        ### RNG used to generate noise properties
+        seed = 545454
+        self.rng = np.random.default_rng(seed=[seed, self.comm.rank])
 
-lbs_sim = lbsim_simulation(nside=16)
+
+lbs_sim = lbsim_simulation()
 
 
 @pytest.mark.parametrize(
     "lbsim_obj",
     [(lbs_sim)],
 )
+@pytest.mark.ignore_param_count
 class TestLBSim_InvNoiseCovLO_UnCorr:
     def test_LBSim_InvNoiseCov_UnCorr_explicit(self, lbsim_obj):
         """Here the noise variances are specified explicitly"""
@@ -140,3 +145,132 @@ class TestLBSim_InvNoiseCovLO_UnCorr:
                 [1.0e4 / obs.test_tod for obs in lbsim_obj.sim.observations], axis=None
             ),
         )
+
+
+@pytest.mark.parametrize(
+    "lbsim_obj",
+    [(lbs_sim)],
+)
+@pytest.mark.ignore_param_count
+class TestLBSim_InvNoiseCovLO_Circulant:
+    def test_LBSim_InvNoiseCov_Circulant_dict(self, lbsim_obj):
+        """Here the noise covariance and power spectrum are supplied for each detector"""
+        covariance_list = {}
+        power_spec_list = {}
+
+        for detector in lbsim_obj.detector_list:
+            covariance = lbsim_obj.rng.random(
+                size=lbsim_obj.sim.observations[0].n_samples
+            )
+            power_spec = np.fft.fft(covariance).real
+
+            covariance = np.fft.ifft(power_spec).real
+            power_spec = np.fft.fft(covariance).real
+
+            covariance_list[detector.name] = covariance
+            power_spec_list[detector.name] = power_spec
+
+        lbsim_inv_cov1 = brahmap.LBSim_InvNoiseCovLO_Circulant(
+            obs=lbsim_obj.sim.observations,
+            input=covariance_list,
+            input_type="covariance",
+        )
+
+        lbsim_inv_cov2 = brahmap.LBSim_InvNoiseCovLO_Circulant(
+            obs=lbsim_obj.sim.observations,
+            input=power_spec_list,
+            input_type="power_spectrum",
+        )
+
+        row_list = []
+        inv_cov_LO_list = []
+        for obs in lbsim_obj.sim.observations:
+            for __, detector in enumerate(obs.name):
+                inv_cov_LO_list.append(
+                    brahmap.InvNoiseCovLO_Circulant(
+                        size=obs.n_samples,
+                        input=covariance_list[detector][: obs.n_samples],
+                        input_type="covariance",
+                    )
+                )
+                row_list.append(obs.n_samples)
+
+        np.testing.assert_equal(lbsim_inv_cov1.row_size, row_list)
+        np.testing.assert_equal(lbsim_inv_cov2.col_size, row_list)
+        np.testing.assert_equal(lbsim_inv_cov1.size, sum(row_list))
+
+        inv_cov1_explicit = lbsim_inv_cov1.to_array()
+        inv_cov2_explicit = lbsim_inv_cov2.to_array()
+
+        start_row_idx = end_row_idx = 0
+        start_col_idx = end_col_idx = 0
+        full_inv_cov = np.zeros_like(inv_cov1_explicit)
+        for idx in range(lbsim_inv_cov1.num_blocks):
+            end_row_idx += row_list[idx]
+            end_col_idx += row_list[idx]
+            inv_cov_block = inv_cov_LO_list[idx]
+            full_inv_cov[
+                start_row_idx:end_row_idx, start_col_idx:end_col_idx
+            ] = inv_cov_block.to_array()
+            start_row_idx = end_row_idx
+            start_col_idx = end_col_idx
+
+        np.testing.assert_allclose(inv_cov1_explicit, full_inv_cov)
+        np.testing.assert_allclose(inv_cov2_explicit, full_inv_cov)
+
+    def test_LBSim_InvNoiseCov_Circulant_array(self, lbsim_obj):
+        """Here only one common noise covariance and power spectrum is supplied"""
+
+        covariance = lbsim_obj.rng.random(size=lbsim_obj.sim.observations[0].n_samples)
+        power_spec = np.fft.fft(covariance).real
+
+        covariance = np.fft.ifft(power_spec).real
+        power_spec = np.fft.fft(covariance).real
+
+        lbsim_inv_cov1 = brahmap.LBSim_InvNoiseCovLO_Circulant(
+            obs=lbsim_obj.sim.observations,
+            input=covariance,
+            input_type="covariance",
+        )
+
+        lbsim_inv_cov2 = brahmap.LBSim_InvNoiseCovLO_Circulant(
+            obs=lbsim_obj.sim.observations,
+            input=power_spec,
+            input_type="power_spectrum",
+        )
+
+        row_list = []
+        inv_cov_LO_list = []
+        for obs in lbsim_obj.sim.observations:
+            for __, __ in enumerate(obs.name):
+                inv_cov_LO_list.append(
+                    brahmap.InvNoiseCovLO_Circulant(
+                        size=obs.n_samples,
+                        input=covariance[: obs.n_samples],
+                        input_type="covariance",
+                    )
+                )
+                row_list.append(obs.n_samples)
+
+        np.testing.assert_equal(lbsim_inv_cov1.row_size, row_list)
+        np.testing.assert_equal(lbsim_inv_cov2.col_size, row_list)
+        np.testing.assert_equal(lbsim_inv_cov1.size, sum(row_list))
+
+        inv_cov1_explicit = lbsim_inv_cov1.to_array()
+        inv_cov2_explicit = lbsim_inv_cov2.to_array()
+
+        start_row_idx = end_row_idx = 0
+        start_col_idx = end_col_idx = 0
+        full_inv_cov = np.zeros_like(inv_cov1_explicit)
+        for idx in range(lbsim_inv_cov1.num_blocks):
+            end_row_idx += row_list[idx]
+            end_col_idx += row_list[idx]
+            inv_cov_block = inv_cov_LO_list[idx]
+            full_inv_cov[
+                start_row_idx:end_row_idx, start_col_idx:end_col_idx
+            ] = inv_cov_block.to_array()
+            start_row_idx = end_row_idx
+            start_col_idx = end_col_idx
+
+        np.testing.assert_allclose(inv_cov1_explicit, full_inv_cov)
+        np.testing.assert_allclose(inv_cov2_explicit, full_inv_cov)
