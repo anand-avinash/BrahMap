76a0a6e1a5a7d0f6c37bf871c08c88249233e5da
Avinash Anand <36325275+anand-avinash@users.noreply.github.com>
2025-09-05 11:55:21 +0200

    the classes BlockDiagNoiseCovLO and BlockDiagInvNoiseCovLO can now accept a dict mapping block size to the noise property as block_input. This enables using a single instance of noise operator multiple times in the block list by reference if the block size and noise properties are same. This saves the memory a lot

--- brahmap/core/noise_ops_block_diag.py
+++ brahmap/core/noise_ops_block_diag.py
@@ -18,7 +18,7 @@ class BlockDiagNoiseCovLO(BaseBlockDiagNoiseCovLinearOperator):
         _description_
     block_size : Union[np.ndarray, List]
         _description_
-    block_input : List[Union[np.ndarray, List]]
+    block_input : Union[List, Dict]
         _description_
     input_type : Literal["covariance", "power_spectrum"], optional
         _description_, by default "power_spectrum"
@@ -32,36 +32,55 @@ class BlockDiagNoiseCovLO(BaseBlockDiagNoiseCovLinearOperator):
         self,
         operator,
         block_size: Union[np.ndarray, List],
-        block_input: List[Union[np.ndarray, List]],
+        block_input: Union[List, Dict],
         input_type: Literal["covariance", "power_spectrum"] = "power_spectrum",
         dtype: DTypeFloat = np.float64,
         extra_kwargs: Dict[str, Any] = {},
     ):
-        MPI_RAISE_EXCEPTION(
-            condition=(len(block_size) != len(block_input)),
-            exception=ValueError,
-            message="The number of blocks listed in `block_size` is different"
-            "from the number of blocks listed in `block_input`",
-        )
+        if isinstance(block_input, list):
+            MPI_RAISE_EXCEPTION(
+                condition=(len(block_size) != len(block_input)),
+                exception=ValueError,
+                message="The number of blocks listed in `block_size` is different"
+                " from the number of blocks provided in `block_input`",
+            )
 
-        block_list = self.__build_blocks(
-            operator=operator,
-            block_size=block_size,
-            block_input=block_input,
-            input_type=input_type,
-            dtype=dtype,
-            extra_kwargs=extra_kwargs,
-        )
+            block_list = self.__build_blocks_from_list(
+                operator=operator,
+                block_size=block_size,
+                block_input=block_input,
+                input_type=input_type,
+                dtype=dtype,
+                extra_kwargs=extra_kwargs,
+            )
+
+        elif isinstance(block_input, dict):
+            block_list = self.__build_blocks_from_dict(
+                operator=operator,
+                block_size=block_size,
+                block_input=block_input,
+                input_type=input_type,
+                dtype=dtype,
+                extra_kwargs=extra_kwargs,
+            )
+
+        else:
+            MPI_RAISE_EXCEPTION(
+                condition=True,
+                exception=ValueError,
+                message="`block_input` must be either a list of arrays or list"
+                " OR a dictionary that maps operator size to an array or a list",
+            )
 
         super(BlockDiagNoiseCovLO, self).__init__(
             block_list=block_list,
         )
 
-    def __build_blocks(
+    def __build_blocks_from_list(
         self,
         operator,
-        block_input,
-        block_size,
+        block_input: List,
+        block_size: Union[np.ndarray, List],
         input_type,
         dtype,
         extra_kwargs,
@@ -76,6 +95,39 @@ class BlockDiagNoiseCovLO(BaseBlockDiagNoiseCovLinearOperator):
                 **extra_kwargs,
             )
             block_list.append(block_op)
+
+        return block_list
+
+    def __build_blocks_from_dict(
+        self,
+        operator,
+        block_input: Dict,
+        block_size: Union[np.ndarray, List],
+        input_type,
+        dtype,
+        extra_kwargs,
+    ):
+        op_dict = {}
+        for shape in block_input.keys():
+            op_dict[shape] = operator(
+                size=shape,
+                input=block_input[shape],
+                input_type=input_type,
+                dtype=dtype,
+                **extra_kwargs,
+            )
+
+        block_list = []
+        for shape in block_size:
+            if shape in op_dict.keys():
+                block_list.append(op_dict[shape])
+            else:
+                MPI_RAISE_EXCEPTION(
+                    condition=True,
+                    exception=ValueError,
+                    message=f"Operator for shape {shape} is missing from the input dictionary",
+                )
+
         return block_list
 
 
@@ -88,7 +140,7 @@ class BlockDiagInvNoiseCovLO(BlockDiagNoiseCovLO):
         _description_
     block_size : Union[np.ndarray, List]
         _description_
-    block_input : List[Union[np.ndarray, List]]
+    block_input : Union[List, Dict]
         _description_
     input_type : Literal["covariance", "power_spectrum"], optional
         _description_, by default "power_spectrum"
@@ -102,7 +154,7 @@ class BlockDiagInvNoiseCovLO(BlockDiagNoiseCovLO):
         self,
         operator,
         block_size: Union[np.ndarray, List],
-        block_input: List[Union[np.ndarray, List]],
+        block_input: Union[List, Dict],
         input_type: Literal["covariance", "power_spectrum"] = "power_spectrum",
         dtype: DTypeFloat = np.float64,
         extra_kwargs: Dict[str, Any] = {},
