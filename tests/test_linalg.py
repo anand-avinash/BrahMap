61ff9b2344c5e72bc53cac61138c4f0ef7dd57cf
Avinash Anand <36325275+anand-avinash@users.noreply.github.com>
2026-04-01 17:27:35 +0200

    added a test for parallel_norm

--- tests/test_linalg.py
+++ tests/test_linalg.py
@@ -4,7 +4,7 @@ import scipy.sparse.linalg
 import pytest
 
 import brahmap
-from brahmap.math.linalg import cg
+from brahmap.math.linalg import cg, parallel_norm
 
 
 @pytest.fixture
@@ -35,3 +35,25 @@ def test_cg_against_scipy(linear_system):
 
     # testing estimated solution
     np.testing.assert_allclose(x_brahmap, x_scipy, rtol=1e-5, atol=1e-5)
+
+
+def test_parallel_norm():
+    if brahmap.MPI_UTILS.rank == 0:
+        rng = np.random.default_rng(seed=8671)
+        arr = rng.random(10)
+    else:
+        arr = None
+
+    arr = brahmap.MPI_UTILS.comm.bcast(arr, root=0)
+
+    local_norm = np.linalg.norm(arr) ** 2
+    exp_global_norm = np.sqrt(local_norm * brahmap.MPI_UTILS.size)
+
+    global_norm = parallel_norm(arr)
+
+    np.testing.assert_approx_equal(global_norm, exp_global_norm)
+
+
+if __name__ == "__main__":
+    pytest.main([f"{__file__}::test_cg_against_scipy", "-v", "-s"])
+    pytest.main([f"{__file__}::test_parallel_norm", "-v", "-s"])
