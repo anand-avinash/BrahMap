d22d38193c5b851187a141a81c9092e7c354dc97
Avinash Anand <36325275+anand-avinash@users.noreply.github.com>
2026-07-23 18:18:16 +0200

    updated test_BlkDiagPrecondLO.py to use global setup fixture

--- tests/test_BlkDiagPrecondLO.py
+++ tests/test_BlkDiagPrecondLO.py
@@ -32,93 +32,29 @@ import py_BlkDiagPrecondLO as bdplo
 import py_ProcessTimeSamples as hpts
 
 
-class InitCommonParams:
-    np.random.seed(6534 + brahmap.MPI_UTILS.rank)
-    npix = 128
-    nsamples_global = npix * 6
+TOLERANCES_1 = {
+    np.float32: {"rtol": 1.5e-4, "atol": 1.0e-5},
+    np.float64: {"rtol": 1.5e-5, "atol": 1.0e-10},
+}
 
-    div, rem = divmod(nsamples_global, brahmap.MPI_UTILS.size)
-    nsamples = div + (brahmap.MPI_UTILS.rank < rem)
-
-    nbad_pixels_global = npix
-    div, rem = divmod(nbad_pixels_global, brahmap.MPI_UTILS.size)
-    nbad_pixels = div + (brahmap.MPI_UTILS.rank < rem)
-
-    pointings_flag = np.ones(nsamples, dtype=bool)
-    bad_samples = np.random.randint(low=0, high=nsamples, size=nbad_pixels)
-    pointings_flag[bad_samples] = False
+TOLERANCES_2 = {
+    np.float32: {"rtol": 1.5e-3, "atol": 1.0e-5},
+    np.float64: {"rtol": 1.5e-5, "atol": 1.0e-10},
+}
 
 
-class InitInt32Params(InitCommonParams):
-    def __init__(self) -> None:
-        super().__init__()
+class TestBlkDiagPrecondLO_I_Cpp:
+    def test_I_cpp(self, setup_scan):
+        initint, initfloat = setup_scan
 
-        self.dtype = np.int32
-        self.pointings = np.random.randint(
-            low=0, high=self.npix, size=self.nsamples, dtype=self.dtype
-        )
-
-
-class InitInt64Params(InitCommonParams):
-    def __init__(self) -> None:
-        super().__init__()
-
-        self.dtype = np.int64
-        self.pointings = np.random.randint(
-            low=0, high=self.npix, size=self.nsamples, dtype=self.dtype
-        )
-
-
-class InitFloat32Params(InitCommonParams):
-    def __init__(self) -> None:
-        super().__init__()
-
-        self.dtype = np.float32
-        self.noise_weights = np.random.random(size=self.nsamples).astype(
-            dtype=self.dtype
-        )
-        self.pol_angles = np.random.uniform(
-            low=-np.pi / 2.0, high=np.pi / 2.0, size=self.nsamples
-        ).astype(dtype=self.dtype)
-
-
-class InitFloat64Params(InitCommonParams):
-    def __init__(self) -> None:
-        super().__init__()
-
-        self.dtype = np.float64
-        self.noise_weights = np.random.random(size=self.nsamples).astype(
-            dtype=self.dtype
-        )
-        self.pol_angles = np.random.uniform(
-            low=-np.pi / 2.0, high=np.pi / 2.0, size=self.nsamples
-        ).astype(dtype=self.dtype)
-
-
-# Initializing the parameter classes
-initint32 = InitInt32Params()
-initint64 = InitInt64Params()
-initfloat32 = InitFloat32Params()
-initfloat64 = InitFloat64Params()
-
-
-@pytest.mark.parametrize(
-    "initint, initfloat, rtol, atol",
-    [
-        (initint32, initfloat32, 1.5e-4, 1.0e-5),
-        (initint64, initfloat32, 1.5e-4, 1.0e-5),
-        (initint32, initfloat64, 1.5e-5, 1.0e-10),
-        (initint64, initfloat64, 1.5e-5, 1.0e-10),
-    ],
-)
-class TestBlkDiagPrecondLO_I_Cpp(InitCommonParams):
-    def test_I_cpp(self, initint, initfloat, rtol, atol):
+        tol = TOLERANCES_1[initfloat.dtype]
+        rtol, atol = tol["rtol"], tol["atol"]
         solver_type = hpts.SolverType.I
 
         PTS = hpts.ProcessTimeSamples(
-            npix=self.npix,
+            npix=initint.npix,
             pointings=initint.pointings,
-            pointings_flag=self.pointings_flag,
+            pointings_flag=initint.pointings_flag,
             solver_type=solver_type,
             noise_weights=initfloat.noise_weights,
             dtype_float=initfloat.dtype,
@@ -138,23 +74,18 @@ class TestBlkDiagPrecondLO_I_Cpp(InitCommonParams):
         np.testing.assert_allclose(cpp_prod, py_prod, rtol=rtol, atol=atol)
 
 
-@pytest.mark.parametrize(
-    "initint, initfloat, rtol, atol",
-    [
-        (initint32, initfloat32, 1.5e-4, 1.0e-5),
-        (initint64, initfloat32, 1.5e-4, 1.0e-5),
-        (initint32, initfloat64, 1.5e-5, 1.0e-10),
-        (initint64, initfloat64, 1.5e-5, 1.0e-10),
-    ],
-)
-class TestBlkDiagPrecondLO_QU_Cpp(InitCommonParams):
-    def test_QU_cpp(self, initint, initfloat, rtol, atol):
+class TestBlkDiagPrecondLO_QU_Cpp:
+    def test_QU_cpp(self, setup_scan):
+        initint, initfloat = setup_scan
+
+        tol = TOLERANCES_1[initfloat.dtype]
+        rtol, atol = tol["rtol"], tol["atol"]
         solver_type = hpts.SolverType.QU
 
         PTS = hpts.ProcessTimeSamples(
-            npix=self.npix,
+            npix=initint.npix,
             pointings=initint.pointings,
-            pointings_flag=self.pointings_flag,
+            pointings_flag=initint.pointings_flag,
             solver_type=solver_type,
             pol_angles=initfloat.pol_angles,
             noise_weights=initfloat.noise_weights,
@@ -175,23 +106,18 @@ class TestBlkDiagPrecondLO_QU_Cpp(InitCommonParams):
         np.testing.assert_allclose(cpp_prod, py_prod, rtol=rtol, atol=atol)
 
 
-@pytest.mark.parametrize(
-    "initint, initfloat, rtol, atol",
-    [
-        (initint32, initfloat32, 1.5e-4, 1.0e-5),
-        (initint64, initfloat32, 1.5e-4, 1.0e-5),
-        (initint32, initfloat64, 1.5e-5, 1.0e-10),
-        (initint64, initfloat64, 1.5e-5, 1.0e-10),
-    ],
-)
-class TestBlkDiagPrecondLO_IQU_Cpp(InitCommonParams):
-    def test_IQU_cpp(self, initint, initfloat, rtol, atol):
+class TestBlkDiagPrecondLO_IQU_Cpp:
+    def test_IQU_cpp(self, setup_scan):
+        initint, initfloat = setup_scan
+
+        tol = TOLERANCES_1[initfloat.dtype]
+        rtol, atol = tol["rtol"], tol["atol"]
         solver_type = hpts.SolverType.IQU
 
         PTS = hpts.ProcessTimeSamples(
-            npix=self.npix,
+            npix=initint.npix,
             pointings=initint.pointings,
-            pointings_flag=self.pointings_flag,
+            pointings_flag=initint.pointings_flag,
             solver_type=solver_type,
             pol_angles=initfloat.pol_angles,
             noise_weights=initfloat.noise_weights,
@@ -212,23 +138,18 @@ class TestBlkDiagPrecondLO_IQU_Cpp(InitCommonParams):
         np.testing.assert_allclose(cpp_prod, py_prod, rtol=rtol, atol=atol)
 
 
-@pytest.mark.parametrize(
-    "initint, initfloat, rtol, atol",
-    [
-        (initint32, initfloat32, 1.5e-4, 1.0e-5),
-        (initint64, initfloat32, 1.5e-4, 1.0e-5),
-        (initint32, initfloat64, 1.5e-5, 1.0e-10),
-        (initint64, initfloat64, 1.5e-5, 1.0e-10),
-    ],
-)
-class TestBlkDiagPrecondLO_I(InitCommonParams):
-    def test_I(self, initint, initfloat, rtol, atol):
+class TestBlkDiagPrecondLO_I:
+    def test_I(self, setup_scan):
+        initint, initfloat = setup_scan
+
+        tol = TOLERANCES_1[initfloat.dtype]
+        rtol, atol = tol["rtol"], tol["atol"]
         solver_type = hpts.SolverType.I
 
         PTS = hpts.ProcessTimeSamples(
-            npix=self.npix,
+            npix=initint.npix,
             pointings=initint.pointings,
-            pointings_flag=self.pointings_flag,
+            pointings_flag=initint.pointings_flag,
             solver_type=solver_type,
             noise_weights=initfloat.noise_weights,
             dtype_float=initfloat.dtype,
@@ -242,23 +163,18 @@ class TestBlkDiagPrecondLO_I(InitCommonParams):
         np.testing.assert_allclose(bdp_array, diag_inv_count, rtol=rtol, atol=atol)
 
 
-@pytest.mark.parametrize(
-    "initint, initfloat, rtol, atol",
-    [
-        (initint32, initfloat32, 1.5e-3, 1.0e-5),
-        (initint64, initfloat32, 1.5e-3, 1.0e-5),
-        (initint32, initfloat64, 1.5e-5, 1.0e-10),
-        (initint64, initfloat64, 1.5e-5, 1.0e-10),
-    ],
-)
-class TestBlkDiagPrecondLO_QU(InitCommonParams):
-    def test_QU(self, initint, initfloat, rtol, atol):
+class TestBlkDiagPrecondLO_QU:
+    def test_QU(self, setup_scan):
+        initint, initfloat = setup_scan
+
+        tol = TOLERANCES_2[initfloat.dtype]
+        rtol, atol = tol["rtol"], tol["atol"]
         solver_type = hpts.SolverType.QU
 
         PTS = hpts.ProcessTimeSamples(
-            npix=self.npix,
+            npix=initint.npix,
             pointings=initint.pointings,
-            pointings_flag=self.pointings_flag,
+            pointings_flag=initint.pointings_flag,
             solver_type=solver_type,
             pol_angles=initfloat.pol_angles,
             noise_weights=initfloat.noise_weights,
@@ -289,23 +205,18 @@ class TestBlkDiagPrecondLO_QU(InitCommonParams):
         np.testing.assert_allclose(bdp_matrix, bdp_test_matrix, rtol=rtol, atol=atol)
 
 
-@pytest.mark.parametrize(
-    "initint, initfloat, rtol, atol",
-    [
-        (initint32, initfloat32, 1.0e-3, 1.0e-5),
-        (initint64, initfloat32, 1.0e-3, 1.0e-5),
-        (initint32, initfloat64, 1.5e-5, 1.0e-10),
-        (initint64, initfloat64, 1.5e-5, 1.0e-10),
-    ],
-)
-class TestBlkDiagPrecondLO_IQU(InitCommonParams):
-    def test_IQU(self, initint, initfloat, rtol, atol):
+class TestBlkDiagPrecondLO_IQU:
+    def test_IQU(self, setup_scan):
+        initint, initfloat = setup_scan
+
+        tol = TOLERANCES_2[initfloat.dtype]
+        rtol, atol = tol["rtol"], tol["atol"]
         solver_type = hpts.SolverType.IQU
 
         PTS = hpts.ProcessTimeSamples(
-            npix=self.npix,
+            npix=initint.npix,
             pointings=initint.pointings,
-            pointings_flag=self.pointings_flag,
+            pointings_flag=initint.pointings_flag,
             solver_type=solver_type,
             pol_angles=initfloat.pol_angles,
             noise_weights=initfloat.noise_weights,
