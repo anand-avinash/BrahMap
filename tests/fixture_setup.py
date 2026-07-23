62f2cb91423a02e82d0ca980046a5fcd55b33858
Avinash Anand <36325275+anand-avinash@users.noreply.github.com>
2026-07-23 17:17:34 +0200

    defined a global fixture for setting the sky scan parameters for pytest

--- tests/fixture_setup.py
+++ tests/fixture_setup.py
@@ -23,3 +23,62 @@ def setup_linalg_tools(request):
             self.vec = np.random.random(size=self.nsamples).astype(dtype=self.dtype)
 
     return InitParams(dtype=dtype)
+
+
+@pytest.fixture(
+    scope="module",
+    params=[
+        (np.int32, np.float32),
+        (np.int64, np.float32),
+        (np.int32, np.float64),
+        (np.int64, np.float64),
+    ],
+)
+def setup_scan(request):
+    import numpy as np
+    import brahmap
+
+    dtype_int, dtype_float = request.param
+
+    class InitCommonParams:
+        def __init__(self) -> None:
+            np.random.seed(54321 + brahmap.MPI_UTILS.rank)
+            self.npix = 128
+            nsamples_global = self.npix * 6
+
+            div, rem = divmod(nsamples_global, brahmap.MPI_UTILS.size)
+            self.nsamples = div + (brahmap.MPI_UTILS.rank < rem)
+
+            nbad_pixels_global = self.npix
+            div, rem = divmod(nbad_pixels_global, brahmap.MPI_UTILS.size)
+            nbad_pixels = div + (brahmap.MPI_UTILS.rank < rem)
+
+            self.pointings_flag = np.ones(self.nsamples, dtype=bool)
+            bad_samples = np.random.randint(low=0, high=self.nsamples, size=nbad_pixels)
+            self.pointings_flag[bad_samples] = False
+
+    class InitIntParams(InitCommonParams):
+        def __init__(self, dtype) -> None:
+            super().__init__()
+            self.dtype = dtype
+            self.pointings = np.random.randint(
+                low=0, high=self.npix, size=self.nsamples, dtype=self.dtype
+            )
+
+    class InitFloatParams(InitCommonParams):
+        def __init__(self, dtype) -> None:
+            super().__init__()
+            self.dtype = dtype
+            self.noise_weights = np.random.random(size=self.nsamples).astype(
+                dtype=self.dtype
+            )
+            self.pol_angles = np.random.uniform(
+                low=-np.pi / 2.0, high=np.pi / 2.0, size=self.nsamples
+            ).astype(dtype=self.dtype)
+            self.vec = np.random.random(size=self.npix * 3).astype(dtype=self.dtype)
+            self.rvec = np.random.random(size=self.nsamples).astype(dtype=self.dtype)
+
+    initint = InitIntParams(dtype=dtype_int)
+    initfloat = InitFloatParams(dtype=dtype_float)
+
+    return initint, initfloat
