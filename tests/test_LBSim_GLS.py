38d9e061703f6381023ff667436c6ed3245ddccd
Avinash Anand <36325275+anand-avinash@users.noreply.github.com>
2026-07-24 19:23:31 +0200

    parameterized test_LBSim_GLS.py in a robust manner

--- tests/test_LBSim_GLS.py
+++ tests/test_LBSim_GLS.py
@@ -23,133 +23,133 @@ litebird_sim = pytest.importorskip(
 import litebird_sim as lbs  # noqa: E402
 
 
-class lbsim_simulation:
-    def __init__(self, nside, dtype_float):
-        self.comm = lbs.MPI_COMM_WORLD
+@pytest.fixture(
+    scope="module",
+    params=[
+        (np.float32, 1.5e-4, 1.0e-5),
+        (np.float64, 1.5e-6, 1.0e-10),
+    ],
+    ids=["float32", "float64"],
+)
+def setup_lbsim(request):
+    dtype_float, rtol, atol = request.param
 
-        try:
-            nthreads = int(os.environ["OMP_NUM_THREADS"])
-        except:  # noqa: E722
-            nthreads = 1
+    class lbsim_simulation:
+        def __init__(self, nside, dtype_float):
+            self.comm = lbs.MPI_COMM_WORLD
 
-        ### Mission params
-        telescope = "MFT"
-        channel = "M1-195"
-        detector_list = [
-            "001_002_030_00A_195_B",
-            "001_002_029_45B_195_B",
-            "001_002_015_15A_195_T",
-            "001_002_047_00A_195_B",
-        ]
+            try:
+                nthreads = int(os.environ["OMP_NUM_THREADS"])
+            except:  # noqa: E722
+                nthreads = 1
 
-        start_time = 51
-        mission_time_days = 10
-        detector_sampling_freq = 1
+            ### Mission params
+            telescope = "MFT"
+            channel = "M1-195"
+            detector_list = [
+                "001_002_030_00A_195_B",
+                "001_002_029_45B_195_B",
+                "001_002_015_15A_195_T",
+                "001_002_047_00A_195_B",
+            ]
+            start_time = 51
+            mission_time_days = 10
+            detector_sampling_freq = 1
 
-        ### Simulation params
-        imo_version = "vPTEP"
-        imo = lbs.Imo(flatfile_location=lbs.PTEP_IMO_LOCATION)
-        sim_seed = 5132
-        map_seed = 4664
-        self.nside = nside
-        self.dtype_float = dtype_float
-        tmp_dir = tempfile.TemporaryDirectory()
+            ### Simulation params
+            imo_version = "vPTEP"
+            imo = lbs.Imo(flatfile_location=lbs.PTEP_IMO_LOCATION)
+            sim_seed = 5132
+            map_seed = 4664
 
-        ### Initializing the Simulation
-        self.sim = lbs.Simulation(
-            base_path=tmp_dir.name,
-            start_time=start_time,
-            duration_s=mission_time_days * 24 * 60 * 60,
-            random_seed=sim_seed,
-            numba_threads=nthreads,
-            numba_threading_layer="omp",
-            mpi_comm=self.comm,
-            imo=imo,
-        )
+            self.nside = nside
+            self.dtype_float = dtype_float
+            tmp_dir = tempfile.TemporaryDirectory()
 
-        ### Instrument definition
-        self.sim.set_instrument(
-            lbs.InstrumentInfo.from_imo(
-                imo,
-                f"/releases/{imo_version}/satellite/{telescope}/instrument_info",
-            )
-        )
-
-        ### Detector list
-        dets = []
-        for n_det in detector_list:
-            det = lbs.DetectorInfo.from_imo(
-                url=f"/releases/{imo_version}/satellite/{telescope}/{channel}/{n_det}/detector_info",
+            ### Initializing the Simulation
+            self.sim = lbs.Simulation(
+                base_path=tmp_dir.name,
+                start_time=start_time,
+                duration_s=mission_time_days * 24 * 60 * 60,
+                random_seed=sim_seed,
+                numba_threads=nthreads,
+                numba_threading_layer="omp",
+                mpi_comm=self.comm,
                 imo=imo,
             )
-            det.sampling_rate_hz = detector_sampling_freq
-            dets.append(det)
 
-        ### Scanning strategy
-        self.sim.set_scanning_strategy(
-            imo_url=f"/releases/{imo_version}/satellite/scanning_parameters/"
-        )
+            ### Instrument definition
+            self.sim.set_instrument(
+                lbs.InstrumentInfo.from_imo(
+                    imo,
+                    f"/releases/{imo_version}/satellite/{telescope}/instrument_info",
+                )
+            )
 
-        ### Create observations
-        comm_size = self.comm.Get_size()
-        if comm_size == 2:
-            n_block_det = 2
-            n_block_time = 1
-            num_of_obs_per_detector = 1
-        elif comm_size == 4:
-            n_block_det = 2
-            n_block_time = 2
-            num_of_obs_per_detector = 3
-        else:
-            n_block_det = 1
-            n_block_time = self.comm.Get_size()
-            num_of_obs_per_detector = 2
+            ### Detector list
+            dets = []
 
-        self.sim.create_observations(
-            detectors=dets,
-            num_of_obs_per_detector=num_of_obs_per_detector,
-            n_blocks_det=n_block_det,
-            n_blocks_time=n_block_time,
-            split_list_over_processes=False,
-            tod_dtype=self.dtype_float,
-        )
+            for n_det in detector_list:
+                det = lbs.DetectorInfo.from_imo(
+                    url=f"/releases/{imo_version}/satellite/{telescope}/{channel}/{n_det}/detector_info",
+                    imo=imo,
+                )
+                det.sampling_rate_hz = detector_sampling_freq
+                dets.append(det)
 
-        ### Ideal half-wave plate
-        # self.sim.set_hwp(
-        #     lbs.IdealHWP(
-        #         self.sim.instrument.hwp_rpm * 2 * np.pi / 60,
-        #     ),  # applies hwp rotation angle to the polarization angle
-        # )
+            ### Scanning strategy
+            self.sim.set_scanning_strategy(
+                imo_url=f"/releases/{imo_version}/satellite/scanning_parameters/"
+            )
 
-        # Grid communicator object
-        self.MPI_COMM_GRID = lbs.MPI_COMM_GRID
+            ### Create observations
+            comm_size = self.comm.Get_size()
 
-        ### Compute pointings
-        self.sim.prepare_pointings()
-        # self.sim.precompute_pointings(pointings_dtype=self.dtype_float)
+            if comm_size == 2:
+                n_block_det = 2
+                n_block_time = 1
+                num_of_obs_per_detector = 1
+            elif comm_size == 4:
+                n_block_det = 2
+                n_block_time = 2
+                num_of_obs_per_detector = 3
+            else:
+                n_block_det = 1
+                n_block_time = self.comm.Get_size()
+                num_of_obs_per_detector = 2
 
-        ### Random maps
-        np.random.seed(map_seed)
-        self.npix = hp.nside2npix(self.nside)
-        self.dummy_map = np.empty([3, self.npix], dtype=self.dtype_float)
-        self.dummy_map[0] = np.random.uniform(low=-7.0, high=7.0, size=self.npix)
-        self.dummy_map[1] = np.random.uniform(low=-5.0, high=5.0, size=self.npix)
-        self.dummy_map[2] = np.random.uniform(low=-3.0, high=3.0, size=self.npix)
+            self.sim.create_observations(
+                detectors=dets,
+                num_of_obs_per_detector=num_of_obs_per_detector,
+                n_blocks_det=n_block_det,
+                n_blocks_time=n_block_time,
+                split_list_over_processes=False,
+                tod_dtype=self.dtype_float,
+            )
+
+            # Grid communicator object
+            self.MPI_COMM_GRID = lbs.MPI_COMM_GRID
+
+            ### Compute pointings
+            self.sim.prepare_pointings()
+
+            ### Random maps
+            np.random.seed(map_seed)
+            self.npix = hp.nside2npix(self.nside)
+            self.dummy_map = np.empty([3, self.npix], dtype=self.dtype_float)
+
+            self.dummy_map[0] = np.random.uniform(low=-7.0, high=7.0, size=self.npix)
+            self.dummy_map[1] = np.random.uniform(low=-5.0, high=5.0, size=self.npix)
+            self.dummy_map[2] = np.random.uniform(low=-3.0, high=3.0, size=self.npix)
+
+    lbsim_obj = lbsim_simulation(16, dtype_float)
+
+    return lbsim_obj, rtol, atol
 
 
-sim_float32 = lbsim_simulation(16, np.float32)
-sim_float64 = lbsim_simulation(16, np.float64)
-
-
-@pytest.mark.parametrize(
-    "lbsim_obj, rtol, atol",
-    [
-        (sim_float32, 1.5e-4, 1.0e-5),
-        (sim_float64, 1.5e-6, 1.0e-10),
-    ],
-)
 class TestLBSimGLS:
-    def test_LBSim_compute_GLS_maps_I(self, lbsim_obj, rtol, atol):
+    def test_LBSim_compute_GLS_maps_I(self, setup_lbsim):
+        lbsim_obj, rtol, atol = setup_lbsim
         brahmap.MPI_UTILS.update_communicator(lbsim_obj.MPI_COMM_GRID.COMM_OBS_GRID)
 
         ### Setting tod arrays zero
@@ -206,7 +206,8 @@ class TestLBSimGLS:
             atol,
         )
 
-    def test_LBSim_compute_GLS_maps_QU(self, lbsim_obj, rtol, atol):
+    def test_LBSim_compute_GLS_maps_QU(self, setup_lbsim):
+        lbsim_obj, rtol, atol = setup_lbsim
         brahmap.MPI_UTILS.update_communicator(lbsim_obj.MPI_COMM_GRID.COMM_OBS_GRID)
 
         ### Setting tod arrays zero
@@ -251,7 +252,8 @@ class TestLBSimGLS:
 
         np.testing.assert_allclose(GLSresults.GLS_maps, input_map, rtol, atol)
 
-    def test_LBSim_compute_GLS_maps_IQU(self, lbsim_obj, rtol, atol):
+    def test_LBSim_compute_GLS_maps_IQU(self, setup_lbsim):
+        lbsim_obj, rtol, atol = setup_lbsim
         brahmap.MPI_UTILS.update_communicator(lbsim_obj.MPI_COMM_GRID.COMM_OBS_GRID)
 
         ### Setting tod arrays zero
