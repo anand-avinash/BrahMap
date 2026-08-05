d0915835e37c164145a1e757c9f8558cd7d52433
Avinash Anand <36325275+anand-avinash@users.noreply.github.com>
2026-08-05 18:43:10 +0200

    added benchmark tests for the shared memory version of process time samples, pointing operator, and gls wrapper

--- benchmarks/test_bench_core.py
+++ benchmarks/test_bench_core.py
@@ -1,6 +1,7 @@
 import pytest
 from brahmap.core import (
     ProcessTimeSamples,
+    SharedMemProcessTimeSamples,
     PointingLO,
     BlockDiagonalPreconditionerLO,
     compute_GLS_maps_from_PTS,
@@ -38,6 +39,22 @@ class TestProcessTimeSamples:
 
         mpi_benchmark(run)
 
+    def test_bench_shmem_process_time_samples(self, mpi_benchmark, data, stype):
+        def run():
+            shm_PTS = SharedMemProcessTimeSamples(
+                npix=data["npix"],
+                pointings=data["pointings"],
+                pointings_flag=data["pointings_flag"],
+                solver_type=stype,
+                pol_angles=data["pol_angles"],
+                noise_weights=data["noise_weights"],
+                update_pointings_inplace=False,
+                nproc_reduce=1,
+            )
+            shm_PTS.free_shmem_arrays()
+
+        mpi_benchmark(run)
+
 
 @pytest.fixture
 def processed_samples(data, stype):
@@ -50,6 +67,22 @@ def processed_samples(data, stype):
         pol_angles=data["pol_angles"],
         noise_weights=data["noise_weights"],
     )
+
+
+@pytest.fixture
+def processed_shmem_samples(data, stype):
+    """Initializes SharedMemProcessTimeSamples for ONLY the current stype."""
+    shm_PTS = SharedMemProcessTimeSamples(
+        npix=data["npix"],
+        pointings=data["pointings"],
+        pointings_flag=data["pointings_flag"],
+        solver_type=stype,
+        pol_angles=data["pol_angles"],
+        noise_weights=data["noise_weights"],
+        nproc_reduce=1,
+    )
+    yield shm_PTS
+    shm_PTS.free_shmem_arrays()
 
 
 @pytest.mark.benchmark(group="core::LinearOperators")
@@ -82,6 +115,38 @@ class TestLinearOperators:
             .random(lo.nrows)
             .astype(
                 processed_samples.dtype_float,
+            )
+        )
+        mpi_benchmark(lo.T.matvec, vec)
+
+    def test_bench_shmem_PointingLO_matvec(
+        self,
+        benchmark,
+        data,
+        processed_shmem_samples,
+    ):
+        lo = PointingLO(processed_shmem_samples)
+        vec = (
+            data["rng"]
+            .random(lo.ncols)
+            .astype(
+                processed_shmem_samples.dtype_float,
+            )
+        )
+        benchmark(lo.matvec, vec)
+
+    def test_bench_shmem_PointingLO_rmatvec(
+        self,
+        mpi_benchmark,
+        data,
+        processed_shmem_samples,
+    ):
+        lo = PointingLO(processed_shmem_samples)
+        vec = (
+            data["rng"]
+            .random(lo.nrows)
+            .astype(
+                processed_shmem_samples.dtype_float,
             )
         )
         mpi_benchmark(lo.T.matvec, vec)
@@ -126,6 +191,32 @@ class TestGLS:
         mpi_benchmark(
             compute_GLS_maps_from_PTS,
             processed_samples,
+            tod,
+            None,
+            gls_params,
+        )
+
+    def test_bench_shmem_compute_GLS_maps(
+        self,
+        mpi_benchmark,
+        data,
+        processed_shmem_samples,
+        stype,
+    ):
+        gls_params = GLSParameters(
+            solver_type=stype,
+            isolver_max_iterations=3,
+        )
+        tod = (
+            data["rng"]
+            .random(data["nsamples"])
+            .astype(
+                processed_shmem_samples.dtype_float,
+            )
+        )
+        mpi_benchmark(
+            compute_GLS_maps_from_PTS,
+            processed_shmem_samples,
             tod,
             None,
             gls_params,
